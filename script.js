eed52c79394f60fe62809bb2c1e74b220af53ba0
lorenmntlla <c.montillad@outlook.com>
2025-05-22 18:54:03 -0300

    Solves task 2 from javascript.info

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const data = prompt("Digite um texto", "") ?? "";
-document.querySelector("#text").textContent = data;
+const URLs = document.querySelectorAll("a[href]");
+
+for (const url of URLs) {
+	const href = url.getAttribute("href");
+	if (!href.includes("://") || href.includes("internal.com")) continue;
+
+	url.setAttribute("data-order-externalurl", "true");
+}
